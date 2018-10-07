ef4beb08a2f5b4102214962e648e74943f0caa71
J <jevongordon@pursuit.org>
2018-10-07 12:35:43 -0400

    Configured IBAction and IBOutlet to send data to a label

--- Basics/ViewController.swift
+++ Basics/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var numberField: UITextField!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        numberLabel.text = ""
+        nameLabel.text = ""
+        
+    }
+    
+    @IBAction func doneButton(_ sender: UIButton) {
+        let nameInput = nameField.text
+        let numberInput = numberField.text
+        numberLabel.text = numberInput
+        nameLabel.text = nameInput
+    }
+    
 }
 
