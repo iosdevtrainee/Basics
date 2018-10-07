41cec89c6f61161e88d58908e4083af6d419362c
J <jevongordon@pursuit.org>
2018-10-07 11:56:14 -0400

    Brand new project

---
+++ Basics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basics
+//
+//  Created by J on 10/7/18.
+//  Copyright © 2018 J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
